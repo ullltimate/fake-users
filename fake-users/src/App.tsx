de2bc6c9a1e0e44528d4f5ab97744637c3aded31
ullltimate <ullltimate.zp@gmail.com>
2023-08-11 05:05:31 +0300

    feat: set region, slider, field

--- fake-users/src/App.tsx
+++ fake-users/src/App.tsx
@@ -2,24 +2,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Container, Row, Button, Table } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import User from './components/User'
+import { useState } from 'react';
 
 function App() {
+  const [region, setRegion] = useState('en_US');
+  const [slider, setSlider] = useState('0');
+  const [field, setField] = useState('');
+  const maxValue = 10;
+  console.log(slider);
+  console.log(field);
+
   return (
     <>
       <Container>
         <Form>
           <Row className='justify-content-end'>
             <Col md={6}>
-              <Form.Select aria-label="Default select example" className='m-3 mw-25 bg-primary text-white'>
-                <option>Select region, default US</option>
+              <Form.Select aria-label="Default select example" className='m-3 mw-25 bg-primary text-white' value={region} onChange={(e) => setRegion(e.target.value)}>
                 <option value="en_US">United States</option>
                 <option value="ru">Россия</option>
                 <option value="uk">Україна</option>
               </Form.Select>
               <Form.Group>
                 <Form.Label className='mx-3'>Mistake</Form.Label>
-                <Form.Range className='mx-3' min={0} max={10} step={0.25} defaultValue={0}/>
-                <Form.Control className='m-3' placeholder="0" type='number' min={0} max={1000} step={0.25}/>
+                <Form.Range className='mx-3' min={0} max={10} step={0.25} value={slider} onChange={(e) => {setSlider(e.target.value); setField(e.target.value)}}/>
+                <Form.Control className='m-3' placeholder="0" type='number' min={0} max={1000} step={0.25} value={field} onChange={(e) => {setField(e.target.value); setSlider(`${Math.min(Number(e.target.value), maxValue)}`)}}/>
               </Form.Group>
               <Row className='mx-3'>
                 <Col className='p-0'>
@@ -28,7 +35,7 @@ function App() {
                   </Form.Group>
                 </Col>
                 <Col>
-                  <Button variant="primary" type="submit">
+                  <Button variant="primary">
                     Random
                   </Button>
                 </Col>
