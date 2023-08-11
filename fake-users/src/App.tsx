ef60fa09d51a9f176d03b53ebdd5328960cdf86a
ullltimate <ullltimate.zp@gmail.com>
2023-08-11 03:24:58 +0300

    feat layout

--- fake-users/src/App.tsx
+++ fake-users/src/App.tsx
@@ -1,33 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Container, Row, Button, Table } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import User from '../components/User'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <Form>
+          <Row className='justify-content-end'>
+            <Col md={6}>
+              <Form.Select aria-label="Default select example" className='m-3 mw-25 bg-primary text-white'>
+                <option>Select region, default US</option>
+                <option value="en_US">United States</option>
+                <option value="ru">Россия</option>
+                <option value="uk">Україна</option>
+              </Form.Select>
+              <Form.Group>
+                <Form.Label className='mx-3'>Mistake</Form.Label>
+                <Form.Range className='mx-3' min={0} max={10} step={0.25} defaultValue={0}/>
+                <Form.Control className='m-3' placeholder="0" type='number' min={0} max={1000} step={0.25}/>
+              </Form.Group>
+              <Row className='mx-3'>
+                <Col className='p-0'>
+                  <Form.Group controlId="formGridEmail">
+                    <Form.Control type="number" placeholder="Enter seed" min={0}/>
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Button variant="primary" type="submit">
+                    Random
+                  </Button>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Form>
+        <Table bordered hover className='my-5'>
+          <thead className='table-primary'>
+            <tr>
+              <th>№</th>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Adress</th>
+              <th>Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>3343dedw33</td>
+              <td>Mark Polo</td>
+              <td>Otto</td>
+              <td>44 57 88 90</td>
+            </tr>
+            <User/>
+          </tbody>
+        </Table>
+      </Container>
     </>
   )
 }
